4ac7547773874959a78b1e6e283d61246dea116f
Adam Wieckert <wiec1369@gmail.com>
2018-03-29 20:47:45 -0500

    Added all the planet images to a directory. Added the data to the JSON, thank you JOHN!

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.log("YAY!");
+
+const printToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML += domString;
+};
+
+const stuffWithJSON = () => {
+    const data = JSON.parse(this.responseText);
+    buildDomString(data.planets);
+    addEventListeners();
+}
+
+const startApplication = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", stuffWithJSON);
+    myRequest.addEventListener("error", WTF);
+    myRequest.open("GET", "planets.json");
+    myRequest.send();
+}
+
+startApplication();
+
+
+
+
